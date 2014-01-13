199f7b859f9a3a4d58f87c1b6e2bac279a373914
no_author <contact@osivia.com>
2014-01-13 11:13:25 +0000

    "Réfactoring" openToutatice / acrennes

--- opentoutatice-ecm-platform-automation/src/main/java/fr/toutatice/ecm/platform/automation/ReloadMntCfg.java
+++ opentoutatice-ecm-platform-automation/src/main/java/fr/toutatice/ecm/platform/automation/ReloadMntCfg.java
@@ -5,6 +5,8 @@ import org.nuxeo.ecm.automation.core.annotations.Context;
 import org.nuxeo.ecm.automation.core.annotations.Operation;
 import org.nuxeo.ecm.automation.core.annotations.OperationMethod;
 
+import fr.toutatice.ecm.platform.core.services.ToutaticeMaintenanceService;
+
 @Operation(id = ReloadMntCfg.ID, 
 	category = Constants.CAT_SERVICES, 
 	label = "Reload the maintenance service configuration", description = "Force reload the whole configuration of the maintenance service. Accepts no parameter. Returns void.")
