47ec2cf11c73ef6a45e1b2232bf1f2635e9f5446
no_author <contact@osivia.com>
2014-01-27 09:24:07 +0000

    Nouvelles valeurs de precedence Seam

---
+++ opentoutatice-ecm-platform-core/src/main/java/fr/toutatice/ecm/platform/core/constants/ExtendedSeamPrecedence.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package fr.toutatice.ecm.platform.core.constants;
+
+
+/**
+ * @author David Chevrier
+ */
+public interface ExtendedSeamPrecedence {
+    
+    int TOUTATICE = org.jboss.seam.annotations.Install.DEPLOYMENT + 1;
+    
+}
