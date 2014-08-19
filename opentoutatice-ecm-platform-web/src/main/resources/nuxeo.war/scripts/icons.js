d1dadffd034aa7bbfc2a6cb4e6f5b1538bfdd003
no_author <contact@osivia.com>
2014-08-19 15:41:41 +0000

    Gestion du sélecteur d'objet de conf + ajout champ masquer titre, fixes #395, #591

--- opentoutatice-ecm-platform-web/src/main/resources/nuxeo.war/scripts/icons.js
+++ opentoutatice-ecm-platform-web/src/main/resources/nuxeo.war/scripts/icons.js
@@ -1,57 +1,39 @@
 /**
- * function of the icon selector wiget
- * 
- * @param fieldname
- *            the hidden field
- * @param listvalue
- *            the value of the current element clicked
- * @param multiSelect
- *            the mode for multiple selections, true or false
- */
-function toggleIcon(element, fieldname, listvalue, iconsGroup) {
-
+* function of the icon selector wiget
+* @param fieldname  the hidden field
+* @param listvalue the value of the current element clicked
+* @param multiSelect the mode for multiple selections, true or false
+*/
+function toggleIcon(element, fieldname, listvalue, iconsGroup, multiSelect) {
+	
+	// get the input hidden field, for persistance
 	hiddenField = document.getElementById(fieldname);
+	
+	// multiple values can be setted
+	if (multiSelect == true) {
+		jQuery(element).toggleClass('ui-state-active'); // set this current element active or inactive
+		
+		// add or remove the value in hidden field
+		if(hiddenField.value.indexOf(listvalue) == -1) {
+			hiddenField.value = hiddenField.value + listvalue + " ";
+		}
+		else {
+			hiddenField.value = hiddenField.value.replace(listvalue+" ", "");
+		}
 
-	if (typeof (iconsGroup) != "undefined" && iconsGroup != "") {
-		jQuery('#group-' + iconsGroup + ' > .ui-state-default').removeClass(
-				'ui-state-active');
-		jQuery(element).addClass('ui-state-active');
-	} else {
-		jQuery('.ui-state-default').removeClass('ui-state-active');
-		jQuery(element).addClass('ui-state-active');
+	}
+	// one value can be setted
+	else {
+		if (typeof (iconsGroup) != "undefined" && iconsGroup != "") { // get the group of buttons
+			
+			// remove all active
+			jQuery('#group-' + iconsGroup + ' > .ui-state-default').removeClass(
+					'ui-state-active');
+			// set this current element active
+			jQuery(element).addClass('ui-state-active');
+			// persist the value clicked
+			hiddenField.value = listvalue;
+		}
 	}
 
-	hiddenField.value = listvalue;
-
 }
-
-///**
-//* function of the icon selector wiget
-//* @param fieldname  the hidden field
-//* @param listvalue the value of the current element clicked
-//* @param multiSelect the mode for multiple selections, true or false
-//*/
-//function toggleIcon(fieldname, listvalue, multiSelect) {
-//	
-//	hiddenField = document.getElementById(fieldname);
-//	
-//	if (multiSelect == true) {
-//		jQuery(this).toggleClass('ui-state-active');
-//		
-//		if(hiddenField.value.indexOf(listvalue) == -1) {
-//			hiddenField.value = hiddenField.value + listvalue + " ";
-//		}
-//		else {
-//			hiddenField.value = hiddenField.value.replace(listvalue+" ", "");
-//		}
-//
-//	}
-//	else {
-//		jQuery('.ui-state-default').removeClass('ui-state-active');
-//		jQuery(this).addClass('ui-state-active');
-//
-//		hiddenField.value = listvalue;
-//
-//	}
-
-//}
