2c06efaa90b8e9239d58ddbc0a4a12ce59a88b19
no_author <contact@osivia.com>
2014-11-04 17:20:29 +0000

    refs #697 Native tiny_mce_init.js is not override anymore Custom RTE are localized in ottc_tiny_mce_init.js which is injected in portal views

---
+++ opentoutatice-ecm-platform-web/src/main/resources/nuxeo.war/tiny_mce/ottc_tiny_mce_init.js
@@ -0,0 +1,81 @@
+var lang = 'fr';
+
+function createTinyMce(clazz) {
+	var textareas = document.getElementsByClassName(clazz);
+	for ( var index = 0; index < textareas.length; index++) {
+		tinyMCE.execCommand('mceAddControl', false, textareas[index].id);
+	}
+}
+
+window.onload = function(e){
+
+tinyMCE.init({
+	editor_selector : "mceEditorMinimalInLine",
+	mode : "specific_textareas",
+	theme : "advanced",
+	plugins : "",
+	language : lang,
+	theme_advanced_resizing : true,
+	height : "25",
+
+	inline_styles : false,
+	formats : {
+		underline : {
+			inline : 'u',
+			exact : false
+		}
+	},
+
+	// Disable <p> and <br> tag generation
+	force_p_newlines : false,
+	forced_root_block : false,
+	invalid_elements : "br",
+	valid_elements : "strong/b,em,u,span[style<text-decoration: underline;]",
+
+	// Img insertion fixes
+	relative_urls : false,
+	remove_script_host : false,
+	skin : "o2k7",
+	skin_variant : "silver",
+	theme_advanced_buttons1 : "bold,italic,underline",
+	theme_advanced_buttons2 : "",
+	theme_advanced_buttons3 : ""
+
+});
+
+createTinyMce("mceEditorMinimalInLine,disableMCEInit");
+
+tinyMCE
+.init({
+	editor_selector : "mceEditorMinimal",
+	mode : "specific_textareas",
+	theme : "advanced",
+	plugins : "",
+	language : lang,
+	theme_advanced_resizing : true,
+	height : "150",
+
+	inline_styles : false,
+	formats : {
+		underline : {
+			inline : 'u',
+			exact : false
+		}
+	},
+
+	valid_elements : "p,br,strong/b,em,u,span[style<text-decoration: underline;]",
+
+	// Img insertion fixes
+	relative_urls : false,
+	remove_script_host : false,
+	skin : "o2k7",
+	skin_variant : "silver",
+	theme_advanced_buttons1 : "bold,italic,underline",
+	theme_advanced_buttons2 : "",
+	theme_advanced_buttons3 : "",
+
+});
+
+createTinyMce("mceEditorMinimal,disableMCEInit");
+
+}
