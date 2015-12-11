4cdfaf7facc2cfe077afcd6e925920154807e580
no_author <contact@osivia.com>
2015-12-11 09:15:04 +0000

    Relooking des fragments, refs #758

---
+++ opentoutatice-ecm-platform-web/src/main/resources/webpage.nuxeo.war/nuxeo.war/scripts/images-links.js
@@ -0,0 +1,46 @@
+// FIXME: use Loïc's method (it certainly doesn't work here becaus of bad jquery js import
+var $JQry = jQuery.noConflict();
+var $picturePath = "text";
+var $targetPath = "text";
+var $targetTitle = "text";
+
+$JQry(window).load(function() {
+	var $inputPath = $JQry("input[type=hidden][id*='picturePath']")[0];
+	$picturePath = $JQry($inputPath);
+
+	if ($picturePath.val()) {
+		previewImg();
+	}
+
+	var $inputLink = $JQry("input[type=hidden][id*='targetPath']")[0];
+	$targetPath = $JQry($inputLink);
+
+	var $inputTitle = $JQry("[id*='targetTitle']")[0];
+	$inputTitle.addEventListener("blur", setManualPath);
+
+	$targetTitle = $JQry($inputTitle);
+	
+	// It is not an external URL
+	if(!$targetTitle.val().contains("/") && $targetTitle.val() != ""){
+		$targetTitle.attr("readonly","readonly");
+	}
+});
+
+function previewImg() {
+	$JQry("#imagePreview").attr("src", $picturePath.val());
+}
+
+function removeTarget() {
+	$targetPath.val("");
+	$targetTitle.val("");
+
+	if($targetTitle.prop("readonly")){
+		$targetTitle.removeAttr("readonly");
+	}
+}
+
+function setManualPath() {
+	if($targetTitle.val() != "" && $targetTitle.val().contains("/")){
+		$targetPath.val($targetTitle.val());
+	}
+}
