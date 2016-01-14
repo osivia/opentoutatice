a5cf9361972d819bff5d6ecda934625f8dfd4e52
no_author <contact@osivia.com>
2016-01-14 17:27:36 +0000

    Refs #5269: tinyMCE image selection

--- opentoutatice-ecm-platform-web/src/main/resources/portalviews.nuxeo.war/nuxeo.war/scripts/bootstrap/bootstrap-nx-widgets.js
+++ opentoutatice-ecm-platform-web/src/main/resources/portalviews.nuxeo.war/nuxeo.war/scripts/bootstrap/bootstrap-nx-widgets.js
@@ -1,10 +1,11 @@
-//jQuery.noConflict();
-
-window.onload = function(e) {
+function customizeNxWidgets() {
 	/* Nx vocabularies widgets */
 	jQuery('.select2-container').removeAttr("style");
 	jQuery('.select2-container > ul.select2-choices > li > input').removeAttr("style");
 	jQuery('#select2-drop-mask').removeAttr("style");
 	jQuery('.select2-search-field > input').removeAttr("style");
 	jQuery('.select2-drop').removeAttr("style");
+	jQuery('li.select2-search-field > input.select2-input').removeAttr("style");
 }
+
+window.addEventListener("load", customizeNxWidgets);
