e7caa5740aad1b14bfc46926bb1e699ce4740e20
no_author <contact@osivia.com>
2016-01-15 15:55:59 +0000

    Widget de saisie des weburl, refs #938

--- opentoutatice-ecm-platform-web-mode/src/main/java/org/osivia/opentoutatice/web/bean/WebModeDocumentActionsBean.java
+++ opentoutatice-ecm-platform-web-mode/src/main/java/org/osivia/opentoutatice/web/bean/WebModeDocumentActionsBean.java
@@ -3,6 +3,7 @@
  */
 package org.osivia.opentoutatice.web.bean;
 
+import org.apache.commons.lang.StringUtils;
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.Install;
 import org.jboss.seam.annotations.Name;
@@ -10,10 +11,13 @@ import org.jboss.seam.annotations.Observer;
 import org.jboss.seam.annotations.Scope;
 import org.nuxeo.ecm.core.api.ClientException;
 import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.DocumentRef;
 import org.nuxeo.ecm.webapp.helpers.EventNames;
+import org.nuxeo.runtime.api.Framework;
 
 import fr.toutatice.ecm.platform.core.constants.ExtendedSeamPrecedence;
 import fr.toutatice.ecm.platform.core.constants.ToutaticeNuxeoStudioConst;
+import fr.toutatice.ecm.platform.services.permalink.PermaLinkService;
 import fr.toutatice.ecm.platform.web.document.ToutaticeDocumentActionsBean;
 
 /**
@@ -40,6 +44,48 @@ public class WebModeDocumentActionsBean extends ToutaticeDocumentActionsBean {
 		boolean folderish = newDocument.hasFacet("Folderish");
 		
 		newDocument.setPropertyValue(ToutaticeNuxeoStudioConst.CST_DOC_XPATH_TOUTATICE_SIM, folderish);
+	}
+	
+	/**
+	 * Generate the full portal url of the document
+	 * @return
+	 */
+	public String getCurrentUrl() {
+		
+		
+		PermaLinkService permalink = Framework.getService(PermaLinkService.class);
+		String url = permalink.getPortalHost();
+		
+		if(!url.endsWith("/")) {
+			url = url.concat("/");
+		}
+		
+		url = url.concat("web/");
+		
+		DocumentModel document = navigationContext.getCurrentDocument();
+		
+		DocumentRef parent = document.getParentRef();
+		String segments = "";
+		while(parent != null) {
+			document = documentManager.getDocument(parent);
+			if(document.getPropertyValue("ottcweb:segment") != null) {
+				if(StringUtils.isNotBlank(document.getPropertyValue("ottcweb:segment").toString())) {
+					segments = document.getPropertyValue("ottcweb:segment").toString().concat("/").concat(segments);
+				}
+			}
+			
+			if(!parent.equals(document.getParentRef())) {
+				parent = document.getParentRef();
+			}
+			else { 
+				parent = null;
+			}
+		}
+		
+		url = url.concat(segments);
+		
+		return url;
+		
 	}
 
 }
