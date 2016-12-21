50f46444b6ce9cf7954d2e915b62cd2d77dc790a
no_author <contact@osivia.com>
2016-12-21 17:08:17 +0000

    Possibility to create a Document with given dublincore properties

--- opentoutatice-ecm-platform-automation/src/main/java/fr/toutatice/ecm/platform/automation/CreateDocument.java
+++ opentoutatice-ecm-platform-automation/src/main/java/fr/toutatice/ecm/platform/automation/CreateDocument.java
@@ -17,6 +17,11 @@
  */
 package fr.toutatice.ecm.platform.automation;
 
+import java.io.IOException;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.apache.commons.lang.StringUtils;
 import org.nuxeo.ecm.automation.core.Constants;
 import org.nuxeo.ecm.automation.core.annotations.Context;
 import org.nuxeo.ecm.automation.core.annotations.Operation;
@@ -25,10 +30,13 @@ import org.nuxeo.ecm.automation.core.annotations.Param;
 import org.nuxeo.ecm.automation.core.collectors.DocumentModelCollector;
 import org.nuxeo.ecm.automation.core.util.DocumentHelper;
 import org.nuxeo.ecm.automation.core.util.Properties;
+import org.nuxeo.ecm.core.api.ClientException;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.DocumentRef;
 import org.nuxeo.ecm.core.api.pathsegment.PathSegmentService;
+
+import fr.toutatice.ecm.platform.core.helper.ToutaticeDocumentHelper;
 
 @Operation(
 		id = CreateDocument.ID,
@@ -40,7 +48,11 @@ import org.nuxeo.ecm.core.api.pathsegment.PathSegmentService;
 public class CreateDocument {
 
 	public static final String ID = "Document.TTCCreate";
-	private static final String PROP_TITLE = "dc:title";
+	
+	/** DublinCore schema prefix. */
+	protected static final String DUBLINCORE_SCHEMA_PREFIX = "dc:";
+	/** Title property key. */
+	protected static final String PROP_TITLE = "dc:title";
 
 	@Context
 	protected CoreSession session;
@@ -55,29 +67,93 @@ public class CreateDocument {
 	protected String name;
 
 	@Param(name = "properties", required = false)
-	protected Properties content;
+	protected Properties properties;
 
 	@OperationMethod(collector = DocumentModelCollector.class)
 	public DocumentModel run(final DocumentModel doc) throws Exception {
-		if (name == null) {
-			if ((content != null) && (content.get(PROP_TITLE) != null)) {
-				name = pathSegmentService.generatePathSegment(content.get(PROP_TITLE));
+	    // Build name from title if any
+		if (this.name == null) {
+			if ((properties != null) && (properties.get(PROP_TITLE) != null)) {
+			    this.name = pathSegmentService.generatePathSegment(properties.get(PROP_TITLE));
 			} else {
-				name = "Untitled";
+			    this.name = "Untitled";
 			}
 		}
 
-		final DocumentModel newDoc = session.createDocumentModel(doc.getPathAsString(), name, type);
+		DocumentModel newDoc = this.session.createDocumentModel(doc.getPathAsString(), this.name, this.type);
 
-		if (content != null) {
-			DocumentHelper.setProperties(session, newDoc, content);
+		if (this.properties != null) {
+		    // Get Dublincore properties to save them silently
+		    // (to disable DublinCoreListener)
+		    Properties dublinCoreProperties = getDublinCoreProperties(this.properties);
+		    
+		    if(dublinCoreProperties.size() > 0){
+		        // Remove DublinCore entries from original properties
+		        Set<String> propertiesKeys = this.properties.keySet();
+		        propertiesKeys.removeAll(dublinCoreProperties.keySet());
+		        
+		        // Creates with given DublincoreProperties
+		        newDoc = create(this.session, newDoc, this.properties, dublinCoreProperties);
+		        
+		    } else {
+	            // Creates normally
+		        newDoc = this.session.createDocument(newDoc);
+	        }
+		    
+		} else {
+		    // Creates
+		    newDoc = this.session.createDocument(newDoc);
 		}
-		return session.createDocument(newDoc);
+		
+		return newDoc;
 	}
 
 	@OperationMethod(collector = DocumentModelCollector.class)
 	public DocumentModel run(final DocumentRef doc) throws Exception {
-		return run(session.getDocument(doc));
+		return run(this.session.getDocument(doc));
+	}
+	
+	/**
+	 * Extract properties of DublinCore schema from given properties.
+	 * 
+	 * @param properties
+	 * @return properties of DublinCore schema
+	 */
+	protected Properties getDublinCoreProperties(Properties properties){
+	    Properties dcProperties = new Properties();
+	    
+	    if(properties != null){
+	        for(Entry<String, String> property : properties.entrySet()){
+	            if(StringUtils.contains(property.getKey(), DUBLINCORE_SCHEMA_PREFIX)){
+	                dcProperties.put(property.getKey(), property.getValue());
+	            }
+	        }
+	    }
+	    return dcProperties;
+	}
+	
+	/**
+	 * Creates document setting Dublincore properties.
+	 * 
+	 * @param session
+	 * @param document
+	 * @param properties
+	 * @param dublinCoreProperties
+	 * @return document
+	 * @throws ClientException
+	 * @throws IOException
+	 */
+	protected DocumentModel create(CoreSession session, DocumentModel document, Properties properties, Properties dublinCoreProperties) throws ClientException, IOException {
+	    // Create document without given dublincore properties:
+	    // DublinCoreListener sets them
+	    DocumentHelper.setProperties(session, document, properties);
+	    DocumentModel createDocument = session.createDocument(document);
+	    
+	    // Set dublincore properties and save silently to shortcut DublinCoreListener
+	    DocumentHelper.setProperties(session, createDocument, dublinCoreProperties);
+	    ToutaticeDocumentHelper.saveDocumentSilently(session, createDocument, false);
+	    
+	    return createDocument;
 	}
 
 }
