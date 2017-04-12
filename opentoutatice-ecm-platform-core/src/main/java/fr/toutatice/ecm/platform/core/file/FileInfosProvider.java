f6e4f02f1fd032f7426b63caf40021a4b3d685ac
no_author <contact@osivia.com>
2017-04-12 08:35:33 +0000

    Refs #1468: Convertible status with toutaticeAny2Pdf converter for now

--- opentoutatice-ecm-platform-core/src/main/java/fr/toutatice/ecm/platform/core/file/FileInfosProvider.java
+++ opentoutatice-ecm-platform-core/src/main/java/fr/toutatice/ecm/platform/core/file/FileInfosProvider.java
@@ -7,19 +7,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
-import org.artofsolving.jodconverter.OfficeDocumentConverter;
 import org.nuxeo.ecm.core.api.Blob;
 import org.nuxeo.ecm.core.api.ClientException;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.blobholder.BlobHolder;
-import org.nuxeo.ecm.platform.convert.ooomanager.OOoManagerService;
-import org.nuxeo.ecm.platform.mimetype.MimetypeDetectionException;
-import org.nuxeo.ecm.platform.mimetype.MimetypeNotFoundException;
+import org.nuxeo.ecm.core.convert.api.ConversionService;
 import org.nuxeo.ecm.platform.mimetype.interfaces.MimetypeRegistry;
 import org.nuxeo.runtime.api.Framework;
 
-import fr.toutatice.ecm.platform.core.helper.ToutaticeDocumentHelper;
 import fr.toutatice.ecm.platform.core.services.infos.provider.DocumentInformationsProvider;
 
 
@@ -34,33 +30,22 @@ public class FileInfosProvider implements DocumentInformationsProvider {
     /** Error on pdf conversion. */
     public static final String ERROR_ON_PDF_CONVERSION = "errorOnPdfConversion";
     /** Converter name. */
-    public static final String ANY_2_PDF_CONVERTER = "any2pdf";
+    public static final String TTC_ANY_2_PDF_CONVERTER = "toutaticeAny2pdf";
 
     /** MimetypeRegistry. */
     private static MimetypeRegistry mimetypeRegistry;
 
-    /** Office converter. */
-    private static OfficeDocumentConverter officeConverter;
+    /** Conversion service. */
+    private static ConversionService conversionService;
 
     /**
-     * Getter for Office converter.
+     * Getter for Libre Office converter.
      */
-    public static OfficeDocumentConverter getOfficeDocumentConverter() {
-        if (officeConverter == null) {
-            OOoManagerService oooManagerService = Framework.getService(OOoManagerService.class);
-            officeConverter = oooManagerService.getDocumentConverter();
+    public static ConversionService getConversionService() {
+        if (conversionService == null) {
+            conversionService = (ConversionService) Framework.getService(ConversionService.class);
         }
-        return officeConverter;
-    }
-
-    /**
-     * Getter for MimetypeRegistry.
-     */
-    public static MimetypeRegistry getMimetypeRegistry() {
-        if (mimetypeRegistry == null) {
-            mimetypeRegistry = (MimetypeRegistry) Framework.getService(MimetypeRegistry.class);
-        }
-        return mimetypeRegistry;
+        return conversionService;
     }
 
     /**
@@ -85,33 +70,13 @@ public class FileInfosProvider implements DocumentInformationsProvider {
                 if ("application/pdf".equals(mimeType)) {
                     convertible = true;
                 } else {
-                    // Use of possible OfficeDocumentConverter inputMimeTyes
-                    OfficeDocumentConverter converter = getOfficeDocumentConverter();
-                    if (converter != null) {
-                        convertible = converter.getFormatRegistry().getFormatByMediaType(mimeType) != null;
+                    // Check if mimeType is supported by converter
+                    convertible = getConversionService().isSourceMimeTypeSupported(TTC_ANY_2_PDF_CONVERTER, mimeType);
 
-                        // If not found, sniff mimeType from Blob (NOT use of srcMimeType of any2pdf converter)
-                        String sniffedMimeType = null;
-                        if (!convertible) {
-                            try {
-                                sniffedMimeType = getMimetypeRegistry().getMimetypeFromBlob(blob);
-                            } catch (MimetypeNotFoundException | MimetypeDetectionException e) {
-                                // MagicMimetype lib (called from getMimetypeFromBlob) can't found mimetype ...
-                                // but we know it is a text mimetype (case seen for ldif)
-                                // Let's force it to text/plain (!)
-                                if (StringUtils.startsWith(mimeType, "text/")) {
-                                    blob.setMimeType("text/plain");
-                                    bh.setBlob(blob);
-                                    // Save adapted document (as BlobHolder)
-                                    ToutaticeDocumentHelper.saveDocumentSilently(coreSession, currentDocument, true);
-                                }
-                            }
-
-                            // Ckeck with OfficeDocumentConverter
-                            convertible = converter.getFormatRegistry().getFormatByMediaType(sniffedMimeType) != null;
-                        }
-                    } else {
-                        infos.put(ERROR_ON_PDF_CONVERSION, true);
+                    // Instead of declare all possible text mimeTypes on toutaticeAny2pdf converter,
+                    // we say that all based text mimeTypes can be convert
+                    if (!convertible && StringUtils.startsWith(mimeType, "text/")) {
+                        convertible = true;
                     }
                 }
 
