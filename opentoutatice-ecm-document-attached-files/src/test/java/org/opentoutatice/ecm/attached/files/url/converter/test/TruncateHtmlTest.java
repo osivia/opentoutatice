476ba462a56c51a7254061bccab84346ea021b7b
no_author <contact@osivia.com>
2017-06-16 15:39:36 +0000

    Fixes #1526: truncate of annoce:resume

--- opentoutatice-ecm-document-attached-files/src/test/java/org/opentoutatice/ecm/attached/files/url/converter/test/TruncateHtmlTest.java
+++ opentoutatice-ecm-document-attached-files/src/test/java/org/opentoutatice/ecm/attached/files/url/converter/test/TruncateHtmlTest.java
@@ -1,11 +1,8 @@
 package org.opentoutatice.ecm.attached.files.url.converter.test;
 
-import org.apache.commons.lang.StringUtils;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.junit.Test;
+
+import fr.toutatice.ecm.platform.core.freemarker.ToutaticeFunctions;
 
 //@RunWith(FeaturesRunner.class)
 //@Features(CoreFeature.class)
@@ -52,62 +49,9 @@ public class TruncateHtmlTest {
                 + "<p>workspace.gif','common:icon-expanded':null,'common:size':null,'file:content':null,'file:filename':null,'webc:welcomeText':null,'webc:useCaptcha':null,'webc:welcomeMedia':null,'webc:theme':'sites','webc:isWebContainer':null,'webc:baseline':null,'webc:template':null,'webc:logo':null,'webc:themePerspective':null,'webc:themePage':'workspace','webc:name':'WS cr&eacute;a ACL 2','webc:moderationType':'aposteriori','webc:url':'ws-crea-acl-2','webc:email':null,'ttcn:picture':null,'publish:sections':[]}]}, index {[nuxeo][doc][b9f96ffd-fedd-44ef-b120-54fb68040082], source[{'ecm:repository':'default','ecm:uuid':'b9f96ffd-fedd-44ef-b120-54fb68040082','ecm:name':'test-caches','ecm:title':'Test Caches','ecm:path':'/default-domain/workspaces/test-caches','ecm:primaryType':'Workspace','ecm:parentId':'65112d8b-a36b-4b91-a46c-95a360595c0b','ecm:currentLifeCycleState':'project','ecm:versionLabel':'0.0','ecm:isCheckedIn':false,'ecm:isProxy':false,'ecm:isVersion':false,'ecm:isLatestVersion':false,'ecm:isLatestMajorVersion':false,'ecm:mixinType':['TTCPortalConfigurable','SuperSpace','Versionable','Folderish','Space','Orderable'],'ecm:tag':[],'ecm:changeToken':'1485939002402','ecm:acl':['test-caches_owner','test-caches_admin','test-caches_writer','test-caches_contributor','test-caches_contributor','test-caches_reader'],'dc:creator':'admin','dc:source':null,'dc:nature':null,'dc:contributors':['admin'],'dc:created':'2016-10-05T13:28:24.10Z','dc:description':null,'dc:rights':null,'dc:subjects':[],'dc:publisher':null,'dc:valid':null,'dc:format':null,'dc:issued':null,'dc:modified':'2017-02-01T08:50:02.40Z','dc:expired':null,'dc:coverage':null,'dc:language':null,'dc:title':'Test Caches','dc:lastContributor':'admin','uid:uid':null,'uid:minor_version':0,'uid:major_version':0,'ttcs:headImage':null,'ttcs:visibility':'INVITATION','ttcs:spaceCommentable':true,'ttcs:catSpaceKey':null,'ttcs:spaceMembers':[{'login':'admin','newsPeriod':'weekly','nextNewsDate':'2017-02-01T08:52:02.33Z','joinedDate':'2016-10-05T13:28:25.00Z','lastNewsDate':'2017-02-01T08:50:01.11Z'}],'ttcs:lstPublicAuth':[],'ttc:queryPart':null,'ttc:selectors':null,'ttc:childrenPageTemplate':null,'ttc:contextualizeExternalContents':false,'ttc:hiddenInNavigation':null,'ttc:isOnline':false,'ttc:showInMenu':true,'ttc:spaceID':'0fb2b8a3-23ec-4b77-a80e-f20076683ae9','ttc:tabOrder':0,'ttc:contextualizeInternalContents':false,'ttc:pageScope':null,'ttc:extensionUrl':null,'ttc:isPreloadedOnLogin':false,'ttc:domainID':'default-domain-55','ttc:webid':'test-caches','ttc:keywords':[],'ttc:editorialTitle':null,'ttc:vignette':null,'ttc:abstract':null,'ttc:pageTemplate':null,'ttc:images':[],'ttc:theme':null,'ttc:explicitUrl':null,'ttc:lstTargetedPublic':[],'ttc:useES':false,'files:files':[],'common:icon':'/icons/workspace.gif','common:icon-expanded':null,'common:size':null,'file:content':null,'file:filename':null,'webc:welcomeText':null,'webc:useCaptcha':null,'webc:welcomeMedia':null,'webc:theme':'sites','webc:isWebContainer':null,'webc:baseline':null,'webc:template':null,'webc:logo':null,'webc:themePerspective':null,'webc:themePage':'workspace','webc:name':'Test Caches','webc:moderationType':'aposteriori','webc:url':'test-caches','webc:email':null,'ttcn:picture':null,'publish:sections':[]}]}, index {[nuxeo][doc][3ad0a13f-2c25-49aa-9fb2-95b8a722b93b], source[{'ecm:repository':'default','ecm:uuid':'3ad0a13f-2c25-49aa-9fb2-95b8a722b93b','ecm:name':'occupation-des-salles','ecm:title':'Occupation des salles','ecm:path':'/default-domain/workspaces/occupation-des-salles','ecm:primaryType':'Workspace','ecm:parentId':'65112d8b-a36b-4b91-a46c-95a360595c0b','ecm:currentLifeCycleState':'project','ecm:versionLabel':'0.0','ecm:isCheckedIn':false,'ecm:isProxy':false,'ecm:isVersion':false,'ecm:isLatestVersion':false,'ecm:isLatestMajorVersion':false,'ecm:mixinType':['TTCPortalConfigurable','SuperSpace','Versionable','Folderish','Space','Orderable'],'ecm:tag':[],'ecm:changeToken':'1485939000106','ecm:acl':['occupation-des-salles_owner','occupation-des-salles_admin','occupation-des-salles_writer','occupation-des-salles_contributor','occupation-des-salles_contributor','occupation-des-salles_reader'],'dc:creator':'admin','dc:source':null,'dc:nature':null,'dc:contributors':['admin'],'dc:created':'2016-09-30T08:25:11.64Z','dc:description':'Planning','dc:rights':null,'dc:subjects':[],'dc:publisher':null,'dc:valid':null,'dc:format':null,'dc:issued':null,'dc:modified':'2017-02-01T08:50:00.10Z','dc:expired':null,'dc:coverage':null,'dc:language':null,'dc:title':'Occupation des salles','dc:lastContributor':'admin','uid:uid':null,'uid:minor_version':0,'uid:major_version':0,'ttcs:headImage':null,'ttcs:visibility':'INVITATION','ttcs:spaceCommentable':true,'ttcs:catSpaceKey':null,'ttcs:spaceMembers':[{'login':'admin','newsPeriod':'weekly','nextNewsDate':'2017-02-01T08:52:00.04Z','joinedDate':'2016-09-30T08:25:12.20Z','lastNewsDate':'2017-02-01T08:50:00.02Z'}],'ttcs:lstPublicAuth':[],'ttc:queryPart':null,'ttc:selectors':null,'ttc:childrenPageTemplate':null,'ttc:contextualizeExternalContents':false,'ttc:hiddenInNavigation':null,'ttc:isOnline':false,'ttc:showInMenu':true,'ttc:spaceID':'0fb2b8a3-23ec-4b77-a80e-f20076683ae9','ttc:tabOrder':0,'ttc:contextualizeInternalContents':false,'ttc:pageScope':null,'ttc:extensionUrl':null,'ttc:isPreloadedOnLogin':false,'ttc:domainID':'default-domain-55','ttc:webid':'occupation-des-salles','ttc:keywords':[],'ttc:editorialTitle':null,'ttc:vignette':null,'ttc:abstract':null,'ttc:pageTemplate':null,'ttc:images':[],'ttc:theme':null,'ttc:explicitUrl':null,'ttc:lstTargetedPublic':[],'ttc:useES':false,'files:files':[],'common:icon':'/icons/workspace.gif','common:icon-expanded':null,'common:size':null,'file:content':null,'file:filename':null,'webc:welcomeText':null,'webc:useCaptcha':null,'webc:welcomeMedia':null,'webc:theme':'sites','webc:isWebContainer':null,'webc:baseline':null,'webc:template':null,'webc:logo':null,'webc:themePerspective':null,'webc:themePage':'workspace','webc:name':'Occupation des salles','webc:moderationType':'aposteriori','webc:url':'occupation-des-salles','webc:email':null,'ttcn:picture':null,'publish:sections':[]}]}]'{}<br />2017-02-01 09:50:02,833 DEBUG [Nuxeo-Event-PostCommit-45][org.nuxeo.elasticsearch.core.ElasticSearchAdminImpl] Refreshing index associated with repo: default{}<br />2017-02-01 09:50:02,913 DEBUG [Nuxeo-Event-PostCommit-45][org.nuxeo.elasticsearch.core.ElasticSearchAdminImpl] Refreshing index done{}</p>";
 
 //        ToutaticeFunctions tf = new ToutaticeFunctions();
-        String truncatedHtml = truncateHTML(html, 300);
+        String truncatedHtml = new ToutaticeFunctions().truncateHTML(html, 300);
+        System.out.println(truncatedHtml);
     }
     
-    public String truncateHTML(String html, int nbChars) {
-        String truncatedHtml = html;
-        System.out.println("HTML before: " + Jsoup.parse(html).text().length());
-
-        if (html != null) {
-            // Document
-            Document document = Jsoup.parse(html);
-            
-            System.out.println("Body?: " + document.getElementsByTag("body"));
-
-            // Get as text to count characters
-            String text = document.text();
-
-            if (text != null) {
-                // Trim
-                //text = StringUtils.trim(text);
-                // Elements
-                Elements elements = document.getAllElements();
-
-                if (elements != null) {
-                    // Truncate if necessary
-                    for (int index = elements.size(); text.length() > nbChars && index > 0; index--) {
-                        // Remove last element (containing text)
-                        Element lastTextElement = elements.get(index -1);
-                        
-                        if (lastTextElement.text() != null) {
-                            // Inner text
-                            String innerText = lastTextElement.text();
-                            System.out.println("== inner: " + innerText.length());
-                            System.out.println("== inner value: " + innerText);
-                            if(innerText.length() > 0){
-                                // Remove element content from text variable
-                                text = StringUtils.substring(text, 0, text.length() - innerText.length());
-                                System.out.println("== " + text.length());
-                            }
-                        }
-
-                        // Remove element from DOM
-                        lastTextElement.remove();
-                    }
-
-                    // Rebuild String
-                    Elements body = document.getElementsByTag("body");
-                    truncatedHtml = body.first().html();
-                    
-                    System.out.println("HTML after: " + Jsoup.parse(truncatedHtml).text().length());
-                    System.out.println("HTML after: " + truncatedHtml);
-                }
-            }
-
-        }
-
-        return truncatedHtml;
-    }
 
 }
