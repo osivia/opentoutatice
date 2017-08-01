114b1fc599e617f7ba9a1c25607959761d55a94d
no_author <contact@osivia.com>
2017-08-01 10:23:52 +0000

    LBI/ import manquant

--- opentoutatice-ecm-platform-automation/src/main/java/fr/toutatice/ecm/platform/automation/blob/AttachBlobs.java
+++ opentoutatice-ecm-platform-automation/src/main/java/fr/toutatice/ecm/platform/automation/blob/AttachBlobs.java
@@ -6,6 +6,7 @@ import org.nuxeo.ecm.automation.core.annotations.Operation;
 import org.nuxeo.ecm.automation.core.annotations.OperationMethod;
 import org.nuxeo.ecm.automation.core.annotations.Param;
 import org.nuxeo.ecm.automation.core.util.BlobList;
+import org.nuxeo.ecm.core.api.Blob;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
 
