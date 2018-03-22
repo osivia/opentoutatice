c3f3827dc266c854344c13d02e80fd2f33646222
no_author <contact@osivia.com>
2018-03-22 18:39:04 +0000

    New way to register context functions helpers

--- opentoutatice-ecm-platform-core/src/main/java/fr/toutatice/ecm/platform/core/components/ToutaticeCoreBundleActivator.java
+++ opentoutatice-ecm-platform-core/src/main/java/fr/toutatice/ecm/platform/core/components/ToutaticeCoreBundleActivator.java
@@ -13,14 +13,13 @@
  *
  *
  * Contributors:
- *   mberhaut1
- *    
+ * mberhaut1
+ * 
  */
 package fr.toutatice.ecm.platform.core.components;
 
 import java.util.List;
 
-import org.nuxeo.ecm.automation.core.scripting.Functions;
 import org.nuxeo.ecm.core.event.EventService;
 import org.nuxeo.ecm.core.event.PostCommitEventListener;
 import org.nuxeo.ecm.platform.ec.notification.NotificationEventListener;
@@ -30,7 +29,6 @@ import org.nuxeo.runtime.api.Framework;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
-import fr.toutatice.ecm.platform.core.freemarker.ToutaticeFunctions;
 import fr.toutatice.ecm.platform.core.helper.ToutaticeEmailHelper;
 
 /**
@@ -39,48 +37,41 @@ import fr.toutatice.ecm.platform.core.helper.ToutaticeEmailHelper;
  * documents sans modifier les méta-données de ceux-ci pour autant (dublincore, ...).
  * 
  * La méthode start() sera appelé au démarrage lors du déploiement du bundle et installera le service
- * ToutaticeEventFilterService. Ainsi, lorsque le framework sera sollicité pour obtenir une 
+ * ToutaticeEventFilterService. Ainsi, lorsque le framework sera sollicité pour obtenir une
  * instance du service EventService, il obtiendra un proxy sur ce dernier.
- *  
+ * 
  * @author mberhaut1
  */
 public class ToutaticeCoreBundleActivator implements BundleActivator {
-	private static Object currentFnInstance = null;
-	
-	@Override
-	public void start(BundleContext context) throws Exception {
-		// sauvegarde de l'ancienne instance Fn
-		currentFnInstance = Functions.getInstance();
-		// instancier le nouvel Fn
-		ToutaticeFunctions localFn = new ToutaticeFunctions();
-		Functions.setInstance(localFn);
-		// initialiser le mailHelper spécifique Toutatice (pour gestion du permalien portail) 
-		installEmailHelper();
-	}
 
-	@Override
-	public void stop(BundleContext context) throws Exception {
-		// restauration de l'ancienne instance Fn 
-		Functions.setInstance(currentFnInstance);
-	}
+    @Override
+    public void start(BundleContext context) throws Exception {
+        // initialiser le mailHelper spécifique Toutatice (pour gestion du permalien portail)
+        installEmailHelper();
+    }
 
-	private void installEmailHelper() throws Exception {
-		ToutaticeEmailHelper toutaticeEmailHelper = new ToutaticeEmailHelper();
-		
-		// récupération du service de notification
-		NotificationService notificationService = (NotificationService) NotificationServiceHelper.getNotificationService();
-		//setter la nouvelle classe helper
-		notificationService.setEmailHelper(toutaticeEmailHelper);
-		
-		//récupérer l' eventListener de notif
-		EventService eventService = Framework.getService(EventService.class);
+    private void installEmailHelper() throws Exception {
+        ToutaticeEmailHelper toutaticeEmailHelper = new ToutaticeEmailHelper();
+
+        // récupération du service de notification
+        NotificationService notificationService = NotificationServiceHelper.getNotificationService();
+        // setter la nouvelle classe helper
+        notificationService.setEmailHelper(toutaticeEmailHelper);
+
+        // récupérer l' eventListener de notif
+        EventService eventService = Framework.getService(EventService.class);
         List<PostCommitEventListener> listeners = eventService.getPostCommitEventListeners();
-		//setter la nouvelle classe helper
+        // setter la nouvelle classe helper
         for (PostCommitEventListener postCommitEventListener : listeners) {
             if (postCommitEventListener.getClass().equals(NotificationEventListener.class)) {
                 ((NotificationEventListener) postCommitEventListener).setEmailHelper(toutaticeEmailHelper);
             }
         }
-		
-	}
+
+    }
+
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        // Nothing
+    }
 }
