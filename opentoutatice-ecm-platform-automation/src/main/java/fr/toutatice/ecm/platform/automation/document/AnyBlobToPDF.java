8f836a2699e07b8cbf0a2ce3b7e0cec05993c7f4
loic <loic@bebop>
2018-10-26 11:19:56 +0200

    #1918 - manage simultaneous or failed conversions

--- opentoutatice-ecm-platform-automation/src/main/java/fr/toutatice/ecm/platform/automation/document/AnyBlobToPDF.java
+++ opentoutatice-ecm-platform-automation/src/main/java/fr/toutatice/ecm/platform/automation/document/AnyBlobToPDF.java
@@ -9,6 +9,7 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -21,6 +22,7 @@ import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.blobholder.BlobHolder;
 import org.nuxeo.ecm.core.convert.api.ConversionException;
 import org.nuxeo.ecm.core.convert.api.ConversionService;
+import org.nuxeo.runtime.api.Framework;
 
 
 /**
@@ -34,6 +36,10 @@ public class AnyBlobToPDF {
     private static final Log log = LogFactory.getLog(AnyBlobToPDF.class);
     /** specific Logger */ 
     private static final Log sofficelog = LogFactory.getLog("soffice");
+    /** current conversions in progress */
+    private static Map<String, Long> conversionsInProgress = new ConcurrentHashMap<String,Long>();
+    /** time to wait before a new conversion */
+    private static Long retryTime = null;
     
     /** Operation's ID. */
     public static final String ID = "Blob.AnyToPDF";
@@ -74,6 +80,11 @@ public class AnyBlobToPDF {
 
         long startConversionDate = new Date().getTime();
         
+        // LBI #1918 - Don't run multiple conversions on the same document.
+        if(AnyBlobToPDF.checkCurrentConversions(doc)) {
+        	return null;
+        }
+        
         if(sofficelog.isDebugEnabled()) {
         	sofficelog.debug("Start conversion of "+doc.getTitle() + " " +doc.getPath());
         }
@@ -99,6 +110,9 @@ public class AnyBlobToPDF {
         	sofficelog.debug("End of conversion of "+doc.getTitle() + " " +doc.getPath() + " (elapsed time : "+elapsed+ " ms.) ");
         }
         
+        
+        conversionsInProgress.remove(doc.getId());
+
         result = pdfBh.getBlob();
 
         String fname = result.getFilename();
@@ -120,6 +134,54 @@ public class AnyBlobToPDF {
         result.setMimeType("application/pdf");
 
         return result;
+    }
+    
+    /**
+     * Retry on failure management.
+     * If document is marked in the map (failed or currently in progress), it is not generated
+     * until the retry time has passed. 
+     * 
+     * @param doc
+     * @return 
+     */
+    private static synchronized boolean checkCurrentConversions(DocumentModel doc)  {
+    	boolean isInProgress = conversionsInProgress.containsKey(doc.getId());
+    	
+    	if(!isInProgress) {
+    		conversionsInProgress.put(doc.getId(), new Date().getTime());
+    	}
+    	else {
+    		
+    		Long started = conversionsInProgress.get(doc.getId());
+    		Long elapsed = new Date().getTime() - started;
+    		    		
+    		if(elapsed > getRetryTime()) {
+    			conversionsInProgress.put(doc.getId(), new Date().getTime());
+    			isInProgress = false;
+    		}
+    		else {
+    			sofficelog.warn(doc.getTitle() + " "+doc.getPath()+" is currently in conversion, since "+elapsed+"ms.");
+    		}
+    		
+    	}
+    	
+    	return isInProgress;
+    }
+    
+    /**
+     * get retry time param
+     * @return retry time param
+     */
+    private static Long getRetryTime() {
+    	if(retryTime == null) {
+    		String property = Framework.getProperty("ottc.converter.toutaticeAny2pdf.retryTime");
+    		if(property == null) {
+    			property = "1800";
+    		}
+    		
+    		retryTime = Long.parseLong(property) * 1000;
+    	}
+    	return retryTime;
     }
 
 }
