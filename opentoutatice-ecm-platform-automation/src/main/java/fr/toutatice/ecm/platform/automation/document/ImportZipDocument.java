cac37c7c8cef435f88cc571b90b600c57221093f
Loïc Billon <loic.billon@gmail.com>
2023-07-17 16:24:19 +0200

    #18196: erreur fichier vide avec numéro

--- opentoutatice-ecm-platform-automation/src/main/java/fr/toutatice/ecm/platform/automation/document/ImportZipDocument.java
+++ opentoutatice-ecm-platform-automation/src/main/java/fr/toutatice/ecm/platform/automation/document/ImportZipDocument.java
@@ -26,7 +26,7 @@ import org.nuxeo.ecm.platform.filemanager.utils.FileManagerUtils;
 public class ImportZipDocument {
 
     public static final String ID = "FileManager.ImportZip";
-    
+
     @Context
     protected AutomationService as;
 
@@ -35,74 +35,89 @@ public class ImportZipDocument {
 
     @Context
     protected FileManager fileManager;
-    
+
     protected DocumentModel getCurrentDocument() throws Exception {
         String cdRef = (String) context.get("currentDocument");
         return as.getAdaptedValue(context, cdRef, DocumentModel.class);
     }
-    
+
     @OperationMethod
     public void run(Blob blob) throws Exception {
         DocumentModel currentDocument = getCurrentDocument();
-        
+
         int indexOfIgnoreCase = StringUtils.indexOfIgnoreCase(blob.getFilename(), ".zip");
         String zipRootFolderName = blob.getFilename().substring(0, indexOfIgnoreCase);
-        
+
         DocumentModel zipRootFolder = fileManager.createFolder(context.getCoreSession(), zipRootFolderName, currentDocument.getPathAsString(), true);
-        
-        
+
+
         File tmp = null;
         try {
             CharsetDetector detector = new CharsetDetector();
             detector.setText(blob.getStream());
             CharsetMatch charsetMatch = detector.detect();
 
-        	tmp = File.createTempFile("importer", null);
-        	blob.transferTo(tmp);
+            tmp = File.createTempFile("importer", null);
+            blob.transferTo(tmp);
 
-        	ZipFile zipFile = new ZipFile(tmp, charsetMatch.getName());
-        	
+            ZipFile zipFile = new ZipFile(tmp, charsetMatch.getName());
+
+            // Pour chaque entrée du zip.
             Enumeration<?> enu = zipFile.getEntries();
             while (enu.hasMoreElements()) {
                 ZipArchiveEntry zipEntry = (ZipArchiveEntry) enu.nextElement();
 
-                String name = zipEntry.getName();
+                String name = zipEntry.getName(); // chemin complet
                 DocumentModel documentFolder = zipRootFolder;
                 String entryFileName = name;
-                if(name.contains("/") ) {
-                    entryFileName = StringUtils.substringAfterLast(name, "/");
-                    String entryDirName = StringUtils.substringBeforeLast(name, "/");
-                    
-                    if(StringUtils.isNotBlank(entryDirName)) {
-                    	
-                    	for(String dir : entryDirName.split("/")) {
-                    		DocumentModel existingDocByTitle = FileManagerUtils.getExistingDocByTitle(context.getCoreSession(), documentFolder.getPathAsString(), dir);
-                    		
-                    		if(existingDocByTitle == null) {
-                    			documentFolder = fileManager.createFolder(context.getCoreSession(), dir, documentFolder.getPathAsString(), true);
-                    		}
-                    		else {
-                    			documentFolder = existingDocByTitle;
-                    		}
-                    	}
-                    	
+
+                // Dossier uniquement
+                if (name.endsWith("/")) {
+                    createFolder(name, zipRootFolder);
+
+                }
+                // Fichier et/ou dossier
+                else {
+
+                    entryFileName = name; // Si fichier directement à la racine
+
+                    if (name.contains("/")) { // Si fichier dans un sous dossier
+                        entryFileName = StringUtils.substringAfterLast(name, "/");
+                        String entryDirName = StringUtils.substringBeforeLast(name, "/");
+
+                        // On tente de créer le sous-dossier si il n'est pas explicite dans l'archive.
+                        documentFolder = createFolder(entryDirName, zipRootFolder);
                     }
+                    InputStream inputStream = zipFile.getInputStream(zipEntry);
+
+                    Blob input = new InputStreamBlob(inputStream);
+
+                    fileManager.createDocumentFromBlob(context.getCoreSession(), input, documentFolder.getPathAsString(), true, entryFileName);
+
                 }
 
-                InputStream inputStream = zipFile.getInputStream(zipEntry);
-                
-                Blob input = new InputStreamBlob(inputStream);
-                
-                fileManager.createDocumentFromBlob(context.getCoreSession(), input, documentFolder.getPathAsString(), true, entryFileName);
-
             }
-            zipFile.close();        	
-        	
-        }
-        finally {
-        	tmp.delete();
+            zipFile.close();
+
+        } finally {
+            tmp.delete();
         }
 
     }
-    
+
+    private DocumentModel createFolder(String zipPath, DocumentModel documentFolder) throws Exception {
+
+        for (String dir : zipPath.split("/")) {
+            DocumentModel existingDocByTitle = FileManagerUtils.getExistingDocByTitle(context.getCoreSession(), documentFolder.getPathAsString(), dir);
+
+            if (existingDocByTitle == null) {
+                documentFolder = fileManager.createFolder(context.getCoreSession(), dir, documentFolder.getPathAsString(), true);
+            } else {
+                documentFolder = existingDocByTitle;
+            }
+        }
+
+        return documentFolder;
+    }
+
 }
